01f724a4d23e978c42901be82e06fc74f9e4c7ba
kidow <wcgo2ling@hanmail.net>
2018-12-03 19:10:22 +0900

    header logout set

--- src/components/Base/Header/Header.js
+++ src/components/Base/Header/Header.js
@@ -2,21 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import './Header.scss'
 
-const Header = ({username}) => {
+const Header = ({username, logged, onLogout}) => {
   return (
     <div className='header'>
       <div className='contents'>
         <div className='left-side'>
           <Link to='/' className='logo'>빙고</Link>
           <Link to='/new' className='new'>새 빙고 만들기</Link>
-          <Link className='my' to={`/user/${username}`}>내가 만든 빙고</Link>
+          <Link className='my' to={`/posts/${username}`}>내가 만든 빙고</Link>
         </div>
         <div className='spacer'>
           <input placeholder='빙고 제목을 입력 후 엔터를 눌러주세요.' />
         </div>
-        <Link to='/auth/login' className='right-side'>
-          로그인 | 가입
-        </Link>
+        {logged ? (
+          <div className='logged'>
+            <Link to={`/user/${username}`} className='info'>수정</Link>
+            <div onClick={onLogout} className='logout'>로그아웃</div>
+          </div>
+        ) : (
+          <Link to='/auth/login' className='right-side'>
+            로그인 | 가입
+          </Link>
+        )}
       </div>
       <div className='gradient-border'/>
     </div>
