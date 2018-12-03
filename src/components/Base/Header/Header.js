ea04c5771be8d53ace0750b468358a5215792b6b
kidow <wcgo2ling@hanmail.net>
2018-12-03 22:24:32 +0900

    로그인 안했을 시 새 빙고를 만들때 로그인화면으로 돌려보낸다

--- src/components/Base/Header/Header.js
+++ src/components/Base/Header/Header.js
@@ -15,8 +15,8 @@ const Header = ({username, logged, onLogout}) => {
       <div className='contents'>
         <div className='left-side'>
           <Link to='/' className='logo'>빙고</Link>
-          <NavLink to='/new' activeStyle={activeStyle} className='new'>새 빙고 만들기</NavLink>
-          {logged && <NavLink className='my' activeStyle={activeStyle} to={`/posts/${username}`}>내가 만든 빙고</NavLink>}
+          <NavLink to={logged ? '/new' : '/auth/login'} activeStyle={activeStyle} className='new'>새 빙고 만들기</NavLink>
+          {logged && <NavLink className='my' activeStyle={logged && activeStyle} to={`/posts/${username}`}>내가 만든 빙고</NavLink>}
         </div>
         <div className='spacer'>
           <input placeholder='빙고 제목을 입력 후 엔터를 눌러주세요.' />
