ec281132c5edfa81b8b28d61700299737223d3ce
kidow <wcgo2ling@hanmail.net>
2018-12-03 19:45:34 +0900

    headerContainer set

--- src/containers/Base/HeaderContainer.js
+++ src/containers/Base/HeaderContainer.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import { Header } from 'components/Base';
 
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as userActions from 'store/user'
+
+import storage from 'lib/storage'
+
 class HeaderContainer extends Component {
+  handleLogout = async () => {
+    const { UserActions } = this.props
+    try {
+      await UserActions.logout()
+    } catch (e) {
+      console.log(e)
+    }
+    storage.remove('loggedInfo')
+    window.location.href = '/'
+  }
+
   render() {
+    const { logged, username } = this.props
+    const { handleLogout } = this
     return (
-      <Header />
+      <Header 
+        logged={logged} 
+        onLogout={handleLogout}
+        username={username}
+      />
     );
   }
 }
 
-export default HeaderContainer;
+export default connect(
+  state => ({
+    logged: state.user.get('logged'),
+    username: state.user.getIn(['loggedInfo', 'username'])
+  }),
+  dispatch => ({
+    UserActions: bindActionCreators(userActions, dispatch)
+  })
+)(HeaderContainer);
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import auth from './auth'
+import user from './user'
 
 import { combineReducers } from 'redux'
 import { penderReducer } from 'redux-pender'
 
 export default combineReducers({
   auth,
+  user,
   penderReducer
 })
