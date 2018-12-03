a9124940272d597141b36ccae075dc6b473f3a3a
kidow <wcgo2ling@hanmail.net>
2018-12-03 19:16:40 +0900

    header inpt letter-spacing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { Home, Auth, New, User, Posts } from 'pages';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/auth' component={Auth}/>
         <Route path='/new' component={New}/>
         <Route path='/user/:username' component={User}/>
         <Route path='/posts/:username' component={Posts}/>
-      </div>
+      </Switch>
     );
   }
 }
