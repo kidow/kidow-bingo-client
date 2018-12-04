93891627e44b2a41cdd6d8516a66a5568ef5576c
kidow <wcgo2ling@hanmail.net>
2018-12-04 15:51:57 +0900

    NewPost table, input, button ui set

---
+++ src/components/Base/Table/Table.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Table.scss'
+
+const Table = () => {
+  return (
+    <div className='table'>
+      <div className='row-1'>
+        <input className='column-1' />
+        <input className='column-2' />
+        <input className='column-3' />
+        <input className='column-4' />
+        <input className='column-5' />
+      </div>
+      <div className='row-2'>
+        <input className='column-1' />
+        <input className='column-2' />
+        <input className='column-3' />
+        <input className='column-4' />
+        <input className='column-5' />
+      </div>
+      <div className='row-3'>
+        <input className='column-1' />
+        <input className='column-2' />
+        <input className='column-3' />
+        <input className='column-4' />
+        <input className='column-5' />
+      </div>
+      <div className='row-4'>
+        <input className='column-1' />
+        <input className='column-2' />
+        <input className='column-3' />
+        <input className='column-4' />
+        <input className='column-5' />
+      </div>
+      <div className='row-5'>
+        <input className='column-1' />
+        <input className='column-2' />
+        <input className='column-3' />
+        <input className='column-4' />
+        <input className='column-5' />
+      </div>
+    </div>
+  );
+};
+
+export default Table;
--- src/components/Shared/NewPost/NewPost.js
+++ src/components/Shared/NewPost/NewPost.js
@@ -1,45 +1,20 @@
 import React from 'react';
 import './NewPost.scss'
+import { Input, Button, Table } from 'components/Base';
 
 const NewPost = () => {
   return (
     <div className='new-post'>
-      <div className='table'>
-        <div className='row-1'>
-          <input className='column-1' />
-          <input className='column-2' />
-          <input className='column-3' />
-          <input className='column-4' />
-          <input className='column-5' />
-        </div>
-        <div className='row-2'>
-          <input className='column-1' />
-          <input className='column-2' />
-          <input className='column-3' />
-          <input className='column-4' />
-          <input className='column-5' />
-        </div>
-        <div className='row-3'>
-          <input className='column-1' />
-          <input className='column-2' />
-          <input className='column-3' />
-          <input className='column-4' />
-          <input className='column-5' />
-        </div>
-        <div className='row-4'>
-          <input className='column-1' />
-          <input className='column-2' />
-          <input className='column-3' />
-          <input className='column-4' />
-          <input className='column-5' />
-        </div>
-        <div className='row-5'>
-          <input className='column-1' />
-          <input className='column-2' />
-          <input className='column-3' />
-          <input className='column-4' />
-          <input className='column-5' />
-        </div>
+      <Table />
+      <div className='post-container'>
+        <Input 
+          label='제목 (필수)' 
+          placeholder='제목을 입력하세요'
+        />
+        <Input 
+          label='한줄 설명'
+        />
+        <Button>완성하기</Button>
       </div>
     </div>
   );
