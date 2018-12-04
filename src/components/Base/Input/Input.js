460c9440be454049ea957bbb82c51df24fc94a99
kidow <wcgo2ling@hanmail.net>
2018-12-04 13:55:07 +0900

    input autocomplete off set

--- src/components/Base/Input/Input.js
+++ src/components/Base/Input/Input.js
@@ -5,7 +5,7 @@ const Input = ({label, ...rest}) => {
   return (
     <div className='input-with-label'>
       <div className='label'>{label}</div>
-      <input className='input' {...rest}/>
+      <input className='input' autoComplete='off' {...rest}/>
     </div>
   );
 };
